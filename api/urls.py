6c6187ae617c2933ebe4beb63a700b2f35728e8b
Easik Inc. <Haneishiteiru@gmail.com>
2025-09-02 16:31:01 +0530

    ini -commit

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('businesses.urls')),
+    path('reviews/', include('reviews.urls')),
+    
 ]

8de39105e379e0d8e6adab91364f0bbfdd9fb6be
Easik Inc. <Haneishiteiru@gmail.com>
2025-09-02 12:24:55 +0000

    m

--- api/settings.py
+++ api/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api.urls'
+ROOT_URLCONF = 'quickrate.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api.wsgi.app'
+WSGI_APPLICATION = 'quickrate.wsgi.app'
 
 
 # Database

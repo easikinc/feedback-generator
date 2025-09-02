c8b8a114dcb6756db249276aab3272fa77d32d65
Easik Inc. <Haneishiteiru@gmail.com>
2025-09-02 11:22:33 +0000

    m

--- api/settings.py
+++ api/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example'
+    'businesses',
+    'reviews',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google'
+    
 ]
 
 MIDDLEWARE = [
